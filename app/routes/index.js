f312f412fb6b4174fa87b5bc039e56d8270d13f4
danielspector <danielyspector@gmail.com>
2014-04-29 19:57:30 -0400

    added reddit api calls for photos

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,10 @@
 export default Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    console.log("hey there");
+    return $.get("http://www.reddit.com/r/funny/new.json").then(function(data){
+      return data.data.children.filter(function(el){
+        return el.data.url.match(/.jpg$|.png$/)
+      });
+    });
   }
 });
